f954e6741867dc67acfc5261db564e7330fa2910
caileyapple <122415798+caileyapple@users.noreply.github.com>
2026-03-10 20:06:40 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function showTab(tabId){
+
+let tabs = document.querySelectorAll(".tab-content");
+
+tabs.forEach(tab=>{
+tab.classList.remove("active");
+});
+
+document.getElementById(tabId).classList.add("active");
+
+}
